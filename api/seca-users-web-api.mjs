24114ad5b67107d989697eba6b4ea5e855cdc402
Tiago M. F. Nevoa <tiagonevoa@gmail.com>
2023-12-02 13:13:06 +0000

    EOF

--- api/seca-users-web-api.mjs
+++ api/seca-users-web-api.mjs
@@ -11,6 +11,7 @@ async function createUser(req, rsp) {
 
   if(secaServices.insertUser(userName)) {
     return rsp.status(201).json({"user-token": user.token})
-  } 
+  }
   rsp.status(400).json("User already exists")
+
 }
