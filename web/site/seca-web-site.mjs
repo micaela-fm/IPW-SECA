61659c32ab240fe4446bcdcdeb9a048b88964f16
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2023-12-16 17:06:16 +0000

    seca-web-site: fixed _getPopularEvents and _searchEventsByName for more coherent response format

--- web/site/seca-web-site.mjs
+++ web/site/seca-web-site.mjs
@@ -28,7 +28,7 @@ export default function (secaEventsServices, secaGroupsServices, secaUsersServic
     const size = req.query.size
     const page = req.query.page
     const events = await secaEventsServices.getPopularEvents(size, page)
-    rsp.render('popularEvents', { events })
+    rsp.render('popularEvents', events)
   }
 
   // Search events by name
