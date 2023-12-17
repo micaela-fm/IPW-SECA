227c48a72aecfc859a996ec90c1a47ce3ac46702
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2023-12-17 17:37:58 +0000

    ES: getUserId, getAllGroups, createGroup

--- data/elasticsearch/seca-data-db.mjs
+++ data/elasticsearch/seca-data-db.mjs
@@ -1,4 +1,5 @@
 import crypto from 'node:crypto'
+import errors from '../../common/errors.mjs'
 import { Client } from '@elastic/elasticsearch'
 import * as fetchWrapper from './fetch-wrapper.mjs'
 import uriManager from './uri-manager.mjs'
@@ -6,6 +7,9 @@ import uriManager from './uri-manager.mjs'
 const client = new Client({ node: process.env.ELASTICSEARCH_NODE || 'http://localhost:9200' })
 
 export default function () {
+    const userUriManager = uriManager('users')
+    const groupUriManager = uriManager('groups')
+    
     return {
         insertUser,
         getUserId,
@@ -24,7 +28,6 @@ export default function () {
             token: crypto.randomUUID()
         }
     
-        const userUriManager = await uriManager('users')
         const uri = userUriManager.create()
         const response = fetchWrapper.post(uri, user)
     
@@ -34,42 +37,41 @@ export default function () {
     }
 
     async function getUserId(userToken) {
-        const result = await client.search({
-            index: 'users_index',
-            body: {
-                _source: ["id"],
-                query: { match: { token: userToken } }
+        const uri = userUriManager.getAll() 
+        const body = {
+            query: {
+                match: { token: userToken }
             }
-        });
-
-		console.log('Result: ', result.body); // Debugging line
-
-        if (result.body.hits.total.value === 0) {
-            throw new Error('User not found.')
         }
-		console.log("Elasticsearch search result:", result); // Debugging line
-        return result.body.hits.hits[0]._source.id
+        const result = await fetchWrapper.post(uri, body)
+
+        if (result.hits.total.value === 0) {
+            throw errors.USER_NOT_FOUND()
+        }
+
+        return result.hits.hits[0]._source.id
     }
 
     async function getAllGroups(userId) {
-        const result = await client.search({
-            index: 'groups_index',
-            body: {
-                _source: ["id"],
-                query: { match: { members: userId } }
+        const uri = groupUriManager.getAll()
+        const body = {
+            query: {
+                match: { userId: userId }
             }
-        });
+        }
+        const result = await fetchWrapper.post(uri, body) 
 
-        if (result.body.hits.total.value === 0) {
-            return [];
+        if (result.hits.total.value === 0) {
+            return []
         }
 
-        return result.body.hits.hits.map(hit => hit._source.id);
+        return result.hits.hits.map(hit => hit._source)
     }
 
+    // TODO
     async function getGroup(groupId) {
         const result = await client.get({
-            index: 'groups_index',
+            index: 'groups',
             id: groupId,
             _source: ["name", "description", "members"]
         });
@@ -81,21 +83,36 @@ export default function () {
         return result.body._source;
     }
 
+    // TODO
     async function createGroup(newGroup) {
-        const response = await client.index({
-            index: 'groups_index',
-            body: newGroup,
-            refresh: true
-        });
-
-        console.log(`Group added successfully with ID ${response.body._id}.`);
-
-        return response.body._id;
+        const group = {
+            name: newGroup.name, 
+            description: newGroup.description, 
+            userId: newGroup.userId, 
+            events: []
+        }
+        const uri = groupUriManager.create()
+        const response = await fetchWrapper.post(uri, group)
+        group.id = response._id
+        return group
     }
 
+    // async function createGroup(newGroup) {
+    //     const response = await client.index({
+    //         index: 'groups_index',
+    //         body: newGroup,
+    //         refresh: true
+    //     });
+
+    //     console.log(`Group added successfully with ID ${response.body._id}.`);
+
+    //     return response.body._id;
+    // }
+
+    // TODO
     async function editGroup(groupId, group) {
         await client.update({
-            index: 'groups_index',
+            index: 'groups',
             id: groupId,
             body: {
                 doc: group
@@ -106,9 +123,10 @@ export default function () {
         console.log(`Group with ID ${groupId} updated successfully.`);
     }
 
+    // TODO
     async function deleteGroup(groupId) {
         const response = await client.delete({
-            index: 'groups_index',
+            index: 'groups',
             id: groupId,
             refresh: true
         });
@@ -126,9 +144,10 @@ export default function () {
         // Implement this function
     }
 
+    // TODO
     async function getUser(username) {
         const result = await client.search({
-            index: 'users_index',
+            index: 'users',
             body: {
                 _source: ["id"],
                 query: { match: { username: username } }
@@ -140,15 +159,5 @@ export default function () {
         }
 
         return result.body.hits.hits[0]._source.id;
-    }
-
-    async function addUser(user) {
-        const response = await client.index({
-            index: 'users_index',
-            body: user,
-            refresh: true
-        });
-
-        return response.body
     }
 }
--- data/elasticsearch/uri-manager.mjs
+++ data/elasticsearch/uri-manager.mjs
@@ -1,13 +1,7 @@
-import { put } from './fetch-wrapper.mjs'
-
 const URI_PREFIX='http://localhost:9200/'
 
 
-export default async function(index) {
-
-    // Create the index unconditionally. If the index already exists, nothing happiness
-    await put(`${URI_PREFIX}${index}`)
-
+export default function(index) {
     return {
         getAll: () => `${URI_PREFIX}${index}/_search`,
         get: (id) => `${URI_PREFIX}${index}/_doc/${id}`,
